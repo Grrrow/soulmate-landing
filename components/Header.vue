503ff45af4fe29e8c90f13e910c335591da1480e
Pablo Carmona <pablocarmonagonzalez@gmail.com>
2023-04-12 19:32:34 +0200

    remove links not used and convert img to home link

--- components/Header.vue
+++ components/Header.vue
@@ -2,15 +2,10 @@
     <header class="bg-gray-800 sticky top-0 z-10">
         <div class="container mx-auto px-4 h-16 flex items-center justify-between">
         <div class="w-16">
-            <img class="w-full h-auto" src="/logo-color.svg" alt="">
+            <a href="#">
+                <img class="w-full h-auto" src="/logo-color.svg" alt="">
+            </a>
         </div>
-        <nav>
-            <ul class="hidden md:flex items-center space-x-4">
-            <li><a href="#" class="text-gray-300 hover:text-white">Home</a></li>
-            <li><a href="#" class="text-gray-300 hover:text-white">Our tech</a></li>
-            <li><a href="#" class="text-gray-300 hover:text-white">Join Us</a></li>
-            </ul>
-        </nav>
         </div>
     </header>
 </template>
