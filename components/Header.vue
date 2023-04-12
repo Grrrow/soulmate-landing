9be0e2b7cdbc003699b12670c78d963dc22a93f6
Pablo Carmona <pablocarmonagonzalez@gmail.com>
2023-04-12 19:09:17 +0200

    resolve merge conflict

--- components/Header.vue
+++ components/Header.vue
@@ -2,13 +2,13 @@
     <header class="bg-gray-800 sticky top-0 z-10">
         <div class="container mx-auto px-4 h-16 flex items-center justify-between">
         <div>
-            <h1>Soulmate</h1>
+            <h1>Solvmate</h1>
         </div>
         <nav>
             <ul class="hidden md:flex items-center space-x-4">
-            <li><a href="#" class="text-gray-300 hover:text-white">Inicio</a></li>
-            <li><a href="#" class="text-gray-300 hover:text-white">Acerca de</a></li>
-            <li><a href="#" class="text-gray-300 hover:text-white">Contacto</a></li>
+            <li><a href="#" class="text-gray-300 hover:text-white">Home</a></li>
+            <li><a href="#" class="text-gray-300 hover:text-white">Our tech</a></li>
+            <li><a href="#" class="text-gray-300 hover:text-white">Join Us</a></li>
             </ul>
         </nav>
         </div>
