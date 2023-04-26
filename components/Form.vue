1ee08c6364764d21fc0fd12d63a29a6e63466f8d
Pablo Carmona <pablocarmonagonzalez@gmail.com>
2023-04-26 18:53:09 +0200

    feat(Form): add validations for inputs and selects

--- components/Form.vue
+++ components/Form.vue
@@ -5,7 +5,7 @@
   >
     <form
       class="max-w-md bg-white p-6 rounded-md shadow-xl"
-      @submit="this.submitForm"
+      @submit.prevent="submitForm"
     >
       <h1 class="text-xl font-semibold mb-8 uppercase text-center">
         Get notified when we launch our Beta version
@@ -19,9 +19,13 @@
             id="first-name"
             type="text"
             v-model="firstName"
-            class="w-full border-b border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 py-2 px-4"
-            required
+            @input="validateFirstName"
+            class="w-full border-b focus:outline-none focus:ring-2 focus:ring-blue-500 py-2 px-4"
+            :class="errors.firstName ? 'border-red-500' : 'border-gray-300'"
           />
+          <p v-if="errors.firstName" class="text-red-500 text-sm">
+            {{ errors.firstName }}
+          </p>
         </div>
         <div>
           <label for="last-name" class="block text-gray-700 font-medium mb-1"
@@ -31,9 +35,13 @@
             id="last-name"
             type="text"
             v-model="lastName"
-            class="w-full border-b border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 py-2 px-4"
-            required
+            @input="validateLastName"
+            class="w-full border-b focus:outline-none focus:ring-2 focus:ring-blue-500 py-2 px-4"
+            :class="errors.lastName ? 'border-red-500' : 'border-gray-300'"
           />
+          <p v-if="errors.lastName" class="text-red-500 text-sm">
+            {{ errors.lastName }}
+          </p>
         </div>
       </div>
       <div class="mt-4">
@@ -44,9 +52,13 @@
           id="email"
           type="email"
           v-model="email"
-          class="w-full border-b border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 py-2 px-4"
-          required
+          @input="validateEmail"
+          class="w-full border-b focus:outline-none focus:ring-2 focus:ring-blue-500 py-2 px-4"
+          :class="errors.email ? 'border-red-500' : 'border-gray-300'"
         />
+        <p v-if="errors.email" class="text-red-500 text-sm">
+          {{ errors.email }}
+        </p>
       </div>
       <div class="mt-4">
         <label for="location" class="block text-gray-700 font-medium mb-1"
@@ -54,11 +66,16 @@
         >
         <multiselect
           v-model="location"
+          @input="validateLocation"
           :options="countries"
           label="name"
           track-by="name"
           placeholder="Select a country"
+          :class="errors.location ? 'location-select-error' : ''"
         ></multiselect>
+        <p v-if="errors.location" class="text-red-500 text-sm">
+          {{ errors.location }}
+        </p>
       </div>
       <div class="mt-4">
         <label
@@ -68,12 +85,17 @@
         >
         <multiselect
           v-model="preferredLanguage"
+          @input="validatePreferredLanguage"
           :options="languages"
           label="name"
           track-by="name"
           placeholder="Select one or more languages"
           :multiple="true"
+          :class="errors.preferredLanguage ? 'preferred-lang-select-error' : ''"
         ></multiselect>
+        <p v-if="errors.preferredLanguage" class="text-red-500 text-sm">
+          {{ errors.preferredLanguage }}
+        </p>
       </div>
       <div class="mt-4">
         <label for="fluent-in" class="block text-gray-700 font-medium mb-1"
@@ -146,6 +168,17 @@ export default {
       agree: false,
       languages: [],
       countries: [],
+      errors: {
+        firstName: "",
+        lastName: "",
+        email: "",
+        location: "",
+        preferredLanguage: "",
+        fluentIn: "",
+        interestedIn: "",
+        reason: "",
+        agree: "",
+      },
     };
   },
   mounted: async function () {
@@ -161,9 +194,54 @@ export default {
         this[collection].push(doc.data());
       });
     },
-    async submitForm(event) {
-      event.preventDefault();
+    validateFirstName() {
+      if (!this.firstName.trim()) {
+        this.errors.firstName = "First Name is required.";
+      } else {
+        this.errors.firstName = "";
+      }
+    },
+    validateLastName() {
+      if (!this.lastName.trim()) {
+        this.errors.lastName = "Last Name is required.";
+      } else {
+        this.errors.lastName = "";
+      }
+    },
+    validateEmail() {
+      const emailPattern = /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/;
+      if (!this.email) {
+        this.errors.email = "Email is required.";
+      } else if (!emailPattern.test(this.email)) {
+        this.errors.email = "Invalid email format.";
+      } else {
+        this.errors.email = "";
+      }
+    },
+    validateLocation() {
+      if (!this.location) {
+        this.errors.location = "Location is required.";
+      } else {
+        this.errors.location = "";
+      }
+    },
+    validatePreferredLanguage() {
+      if (!this.preferredLanguage.length) {
+        this.errors.preferredLanguage = "At least one preferred language is required.";
+      } else {
+        this.errors.preferredLanguage = "";
+      }
+    },
+    async submitForm() {
+      this.validateFirstName();
+      this.validateLastName();
+      this.validateEmail();
+      this.validateLocation();
+      this.validatePreferredLanguage();
 
+      if (this.errors.firstName || this.errors.lastName || this.errors.email || this.errors.location || this.errors.preferredLanguage) {
+        return;
+      }
       await this.$fire.firestore.collection("waitlist").add({
         firstName: this.firstName,
         lastName: this.lastName,
@@ -192,15 +270,20 @@ export default {
   },
 };
 </script>
-<style src="vue-multiselect/dist/vue-multiselect.min.css"></style>
-<style scoped>
+<style>
+@import url("vue-multiselect/dist/vue-multiselect.min.css");
 #form {
   background-color: #d1e6e2;
 }
+
+.location-select-error .multiselect__tags,
+.preferred-lang-select-error .multiselect__tags {
+  @apply border border-red-500;
+}
+
 .notify-btn {
   background-color: #f9a858;
 }
-
 .notify-btn:hover {
   background-color: #f59434;
 }
